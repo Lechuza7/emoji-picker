63a80edc366b9f69433f3c8d50e3e9b692028644
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-08-26 13:57:33 +0200

    handleClickEmoji

--- src/components/emojiPicker/EmojiPicker.jsx
+++ src/components/emojiPicker/EmojiPicker.jsx
@@ -1,7 +1,63 @@
+import { forwardRef, useState } from "react";
+import { data as emojiList } from "./data";
+import EmojiSearch from "./EmojiSearch";
+import EmojiButton from "./EmojiButton";
 
-export default function EmojiPicker() {
+export function EmojiPicker(props, inputRef) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [emojis, setEmojis] = useState(emojiList);
+
+  function handleClickOpen() {
+    setIsOpen(!isOpen);
+  }
+
+  function handleSearch(e) {
+    const query = e.target.value.toLowerCase();
+
+    if (query) {
+      const search = emojiList.filter((emoji) => {
+        return (
+          emoji.name.toLowerCase().includes(query) ||
+          emoji.keywords.toLowerCase().includes(query)
+        );
+      });
+
+      setEmojis(search)
+    } else {
+      setEmojis(emojiList)
+    }
+  }
+  
+  function handleOnClickEmoji(emoji) {
+      const cursorPosition = inputRef.current.selectionStart
+      const text = inputRef.current.value 
+      const prev = text.slice(0, cursorPosition)
+      const next = text.slice(cursorPosition)
+
+      inputRef.current.value = prev + emoji.symbol + next
+      inputRef.current.selectionStart = cursorPosition + emoji.symbol.length
+      inputRef.current.selectionEnd = cursorPosition + emoji.symbol.length
+      inputRef.current.focus()
+  }
 
   return (
-    <div></div>
-  )
+    <div>
+      <button onClick={handleClickOpen}>😊</button>
+
+      {isOpen ? (
+        <div>
+        <EmojiSearch onSearch={handleSearch} />
+        <div>
+          {emojis.map((emoji) => (
+            <EmojiButton key={emoji.symbol} emoji={emoji} onClick={handleOnClickEmoji} />
+          ))}
+        </div>
+      </div>
+      ) : (
+        ""
+        )}
+    </div>
+  );
 }
+
+export default forwardRef(EmojiPicker);
--- src/components/emojiPicker/EmojiPickerInput.jsx
+++ src/components/emojiPicker/EmojiPickerInput.jsx
@@ -4,15 +4,11 @@ import EmojiPicker from "./EmojiPicker";
 export default function EmojiPickerInput() {
   const refInput = useRef(null)
 
-  function handleClick() {
-    refInput.current.focus()
-  }
 
   return(
     <div>
       <input ref={refInput} />
-      <button onClick={handleClick}>Click para focalizar en input</button>
-      <EmojiPicker />
+      <EmojiPicker ref={refInput} />
     </div>
   )
 }
