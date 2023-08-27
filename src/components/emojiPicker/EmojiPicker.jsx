6bc83dde82ff1dd0c4071d42aaaed02c8506e472
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-08-27 10:54:00 +0200

    Container closing with event listener

--- src/components/emojiPicker/EmojiPicker.jsx
+++ src/components/emojiPicker/EmojiPicker.jsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 import { data as emojiList } from "./data";
 import EmojiSearch from "./EmojiSearch";
 import EmojiButton from "./EmojiButton";
@@ -6,6 +6,17 @@ import EmojiButton from "./EmojiButton";
 export function EmojiPicker(props, inputRef) {
   const [isOpen, setIsOpen] = useState(false);
   const [emojis, setEmojis] = useState(emojiList);
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener('click', (e) => {
+      if (!containerRef.current.contains(e.target)) {
+        setIsOpen(false)
+        setEmojis(emojiList)
+      }
+    })
+  }, [])
 
   function handleClickOpen() {
     setIsOpen(!isOpen);
@@ -22,40 +33,44 @@ export function EmojiPicker(props, inputRef) {
         );
       });
 
-      setEmojis(search)
+      setEmojis(search);
     } else {
-      setEmojis(emojiList)
+      setEmojis(emojiList);
     }
   }
-  
-  function handleOnClickEmoji(emoji) {
-      const cursorPosition = inputRef.current.selectionStart
-      const text = inputRef.current.value 
-      const prev = text.slice(0, cursorPosition)
-      const next = text.slice(cursorPosition)
 
-      inputRef.current.value = prev + emoji.symbol + next
-      inputRef.current.selectionStart = cursorPosition + emoji.symbol.length
-      inputRef.current.selectionEnd = cursorPosition + emoji.symbol.length
-      inputRef.current.focus()
+  function handleOnClickEmoji(emoji) {
+    const cursorPosition = inputRef.current.selectionStart;
+    const text = inputRef.current.value;
+    const prev = text.slice(0, cursorPosition);
+    const next = text.slice(cursorPosition);
+
+    inputRef.current.value = prev + emoji.symbol + next;
+    inputRef.current.selectionStart = cursorPosition + emoji.symbol.length;
+    inputRef.current.selectionEnd = cursorPosition + emoji.symbol.length;
+    inputRef.current.focus();
   }
 
   return (
-    <div>
+    <div ref={containerRef}>
       <button onClick={handleClickOpen}>😊</button>
 
       {isOpen ? (
         <div>
-        <EmojiSearch onSearch={handleSearch} />
-        <div>
-          {emojis.map((emoji) => (
-            <EmojiButton key={emoji.symbol} emoji={emoji} onClick={handleOnClickEmoji} />
-          ))}
+          <EmojiSearch onSearch={handleSearch} />
+          <div>
+            {emojis.map((emoji) => (
+              <EmojiButton
+                key={emoji.symbol}
+                emoji={emoji}
+                onClick={handleOnClickEmoji}
+              />
+            ))}
+          </div>
         </div>
-      </div>
       ) : (
         ""
-        )}
+      )}
     </div>
   );
 }
